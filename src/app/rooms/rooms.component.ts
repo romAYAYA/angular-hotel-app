ce99a5f79521e73be23f69c6b21aef7baa5094d5
Roman Khabayev <supernaturalmlady@gmail.com>
2023-08-05 17:21:33 +0600

    ts interface added, ngif, ngfor logic added

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,22 +1,63 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Room, RoomList } from './rooms';
+
 @Component({
   selector: 'hinv-rooms',
   templateUrl: './rooms.component.html',
   styleUrls: ['./rooms.component.scss'],
 })
 export class RoomsComponent implements OnInit {
-  hotelName = 'Hilton Hotel'
+  hotelName = 'Hilton Hotel';
 
-  numberOfRooms = 10
+  numberOfRooms = 10;
 
-  hideRooms = false
+  hideRooms = false;
+
+  rooms: Room = {
+    totalRooms: 20,
+    availableRooms: 10,
+    bookedRooms: 5,
+  };
+
+  roomList: RoomList[] = [
+    {
+      roomNumber: 1,
+      roomType: 'Deluxe Room',
+      amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathrooom, Kitchen',
+      price: 500,
+      photos:
+        'https://images.unsplash.com/photo-1690702691836-c5409092ad01?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+      checkInTime: new Date('11-Nov-2023'),
+      checkOutTime: new Date('12-Nov-2023'),
+    },
+    {
+      roomNumber: 2,
+      roomType: 'Deluxe Room',
+      amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathrooom, Kitchen',
+      price: 1000,
+      photos:
+        'https://images.unsplash.com/photo-1690702691836-c5409092ad01?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+      checkInTime: new Date('11-Nov-2023'),
+      checkOutTime: new Date('12-Nov-2023'),
+    },
+    {
+      roomNumber: 3,
+      roomType: 'Private Suite',
+      amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathrooom, Kitchen',
+      price: 15000,
+      photos:
+        'https://images.unsplash.com/photo-1690702691836-c5409092ad01?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+      checkInTime: new Date('11-Nov-2023'),
+      checkOutTime: new Date('12-Nov-2023'),
+    },
+  ];
 
   constructor() {}
 
   ngOnInit(): void {}
 
   toggle() {
-    this.hideRooms = !this.hideRooms
+    this.hideRooms = !this.hideRooms;
   }
 }
