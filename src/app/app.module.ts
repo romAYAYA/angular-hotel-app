e6c204033f17a21edef702fdf08db00bc024e222
Roman Khabayev <supernaturalmlady@gmail.com>
2023-08-10 20:07:04 +0600

    rooms list component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RoomsComponent } from './rooms/rooms.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RoomsListComponent } from './rooms/rooms-list/rooms-list.component';
 
 @NgModule({
-  declarations: [AppComponent, RoomsComponent],
+  declarations: [AppComponent, RoomsComponent, RoomsListComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -20,45 +20,47 @@ export class RoomsComponent implements OnInit {
     bookedRooms: 5,
   };
 
-  roomList: RoomList[] = [
-    {
-      roomNumber: 1,
-      roomType: 'Deluxe Room',
-      amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathrooom, Kitchen',
-      price: 500,
-      photos:
-        'https://images.unsplash.com/photo-1690702691836-c5409092ad01?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
-      checkInTime: new Date('11-Nov-2023'),
-      checkOutTime: new Date('12-Nov-2023'),
-      rating: 4.5123,
-    },
-    {
-      roomNumber: 2,
-      roomType: 'Deluxe Room',
-      amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathrooom, Kitchen',
-      price: 1000,
-      photos:
-        'https://images.unsplash.com/photo-1690702691836-c5409092ad01?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
-      checkInTime: new Date('11-Nov-2023'),
-      checkOutTime: new Date('12-Nov-2023'),
-      rating: 2.82345,
-    },
-    {
-      roomNumber: 3,
-      roomType: 'Private Suite',
-      amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathrooom, Kitchen',
-      price: 15000,
-      photos:
-        'https://images.unsplash.com/photo-1690702691836-c5409092ad01?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
-      checkInTime: new Date('11-Nov-2023'),
-      checkOutTime: new Date('12-Nov-2023'),
-      rating: 3.59876,
-    },
-  ];
+  roomList: RoomList[] = [];
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.roomList = [
+      {
+        roomNumber: 1,
+        roomType: 'Deluxe Room',
+        amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathrooom, Kitchen',
+        price: 500,
+        photos:
+          'https://images.unsplash.com/photo-1690702691836-c5409092ad01?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+        checkInTime: new Date('11-Nov-2023'),
+        checkOutTime: new Date('12-Nov-2023'),
+        rating: 4.5123,
+      },
+      {
+        roomNumber: 2,
+        roomType: 'Deluxe Room',
+        amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathrooom, Kitchen',
+        price: 1000,
+        photos:
+          'https://images.unsplash.com/photo-1690702691836-c5409092ad01?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+        checkInTime: new Date('11-Nov-2023'),
+        checkOutTime: new Date('12-Nov-2023'),
+        rating: 2.82345,
+      },
+      {
+        roomNumber: 3,
+        roomType: 'Private Suite',
+        amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathrooom, Kitchen',
+        price: 15000,
+        photos:
+          'https://images.unsplash.com/photo-1690702691836-c5409092ad01?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+        checkInTime: new Date('11-Nov-2023'),
+        checkOutTime: new Date('12-Nov-2023'),
+        rating: 3.59876,
+      },
+    ];
+  }
 
   toggle() {
     this.hideRooms = !this.hideRooms;
